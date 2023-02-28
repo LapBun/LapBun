d50205ba1f02bfce9babe43234996a4aa70cda48
kienltgcc200184 <kienltgcc200184@fpt.edu.vn>
2023-02-28 22:34:55 +0700

    Create add product function

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -42,9 +42,36 @@ class ProductController extends AbstractController
         $p = new Product();
         $form = $this->createForm(ProductType::class, $p);
 
-       
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($p->getCreated() === null) {
+                $p->setCreated(new \DateTime());
+            }
+            $imgFile = $form->get('file')->getData();
+            if ($imgFile) {
+                $newFilename = $this->uploadImage($imgFile, $slugger);
+                $p->setImage($newFilename);
+            }
+            $this->repo->save($p, true);
+            return $this->redirectToRoute('product_show', [], Response::HTTP_SEE_OTHER);
+        }
         return $this->render("product/form.html.twig", [
             'form' => $form->createView()
         ]);
+    }
+    public function uploadImage($imgFile, SluggerInterface $slugger): ?string
+    {
+        $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $slugger->slug($originalFilename);
+        $newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile->guessExtension();
+        try {
+            $imgFile->move(
+                $this->getParameter('image_dir'),
+                $newFilename
+            );
+        } catch (FileException $e) {
+            echo $e;
+        }
+        return $newFilename;
     }
 }
