9bc09d3a43aa3b822b69f3b2047ec3cf69a4f144
An200169 <anttgcc200169@fpt.edu.vn>
2023-03-01 19:41:56 +0700

    backup route edit,delete to product controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
-
 /**
  * @Route("/product")
  */
@@ -20,9 +19,9 @@ class ProductController extends AbstractController
 {
     private ProductRepository $repo;
     public function __construct(ProductRepository $repo)
-    {
-        $this->repo = $repo;
-    }
+   {
+      $this->repo = $repo;
+   }
     /**
      * @Route("/", name="product_show")
      */
@@ -30,40 +29,79 @@ class ProductController extends AbstractController
     {
         $products = $this->repo->findAll();
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products'=>$products
         ]);
     }
 
-    /**
+     /**
+     * @Route("/{id}", name="product_read",requirements={"id"="\d+"})
+     */
+    public function showAction(Product $p): Response
+    {
+        return $this->render('detail.html.twig', [
+            'p'=>$p
+        ]);
+    }
+
+     /**
      * @Route("/add", name="product_create")
      */
     public function createAction(Request $req, SluggerInterface $slugger): Response
     {
-        $p = new Product();
-        $form = $this->createForm(ProductType::class, $p);
+        
+    $p = new Product();
+    $form = $this->createForm(ProductType::class, $p);
+
+    $form->handleRequest($req);
+    if($form->isSubmitted() && $form->isValid()){
+        if($p->getCreated()===null){
+            $p->setCreated(new \DateTime());
+        }
+        $imgFile = $form->get('file')->getData();
+        if ($imgFile) {
+            $newFilename = $this->uploadImage($imgFile,$slugger);
+            $p->setImage($newFilename);
+        }
+        $this->repo->save($p,true);
+        return $this->redirectToRoute('product_show', [], Response::HTTP_SEE_OTHER);
+    }
+    return $this->render("product/form.html.twig",[
+        'form' => $form->createView()
+    ]);
+    }   
+
+    /**
+     * @Route("/edit/{id}", name="product_edit",requirements={"id"="\d+"})
+     */
+    public function editAction(Request $req, Product $p,
+    SluggerInterface $slugger): Response
+    {
+        
+        $form = $this->createForm(ProductType::class, $p);   
 
         $form->handleRequest($req);
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($p->getCreated() === null) {
+        if($form->isSubmitted() && $form->isValid()){
+
+            if($p->getCreated()===null){
                 $p->setCreated(new \DateTime());
             }
             $imgFile = $form->get('file')->getData();
             if ($imgFile) {
-                $newFilename = $this->uploadImage($imgFile, $slugger);
+                $newFilename = $this->uploadImage($imgFile,$slugger);
                 $p->setImage($newFilename);
             }
-            $this->repo->save($p, true);
+            $this->repo->save($p,true);
             return $this->redirectToRoute('product_show', [], Response::HTTP_SEE_OTHER);
         }
-        return $this->render("product/form.html.twig", [
+        return $this->render("product/form.html.twig",[
             'form' => $form->createView()
         ]);
     }
-    public function uploadImage($imgFile, SluggerInterface $slugger): ?string
-    {
+
+    public function uploadImage($imgFile, SluggerInterface $slugger): ?string{
         $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $slugger->slug($originalFilename);
-        $newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile->guessExtension();
+        $newFilename = $safeFilename.'-'.uniqid().'.'.$imgFile->guessExtension();
         try {
             $imgFile->move(
                 $this->getParameter('image_dir'),
@@ -79,7 +117,7 @@ class ProductController extends AbstractController
      * @Route("/delete/{id}",name="product_delete",requirements={"id"="\d+"})
      */
 
-    public function deleteAction(Product $p): Response
+    public function deleteAction(Request $request, Product $p): Response
     {
         $this->repo->remove($p,true);
         return $this->redirectToRoute('product_show', [], Response::HTTP_SEE_OTHER);
