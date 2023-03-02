c416073795635516b02ccf32628f8915e7b563f6
AlgerHowrd <hienhngcc200138@fpt.edu.vn>
2023-03-02 10:57:31 +0700

    edit index

--- migrations/Version20230301184230.php
+++ migrations/Version20230301184230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230228165953 extends AbstractMigration
+final class Version20230301184230 extends AbstractMigration
 {
     public function getDescription(): string
     {
